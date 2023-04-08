fad7b49500c6823c851491636a61bd136375ba46
Rafael Flores Souza <raflso@icloud.com>
2023-04-08 12:18:10 -0600

    Finished sidebar ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,28 @@
-import React from "react";
+import Sidebar from "./components/Sidebar";
+import styled from "styled-components";
+import { ThemeProvider } from "./contexts/ThemeContext";
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <ThemeProvider>
+        <Sidebar></Sidebar>
+        <Tmp></Tmp>
+      </ThemeProvider>
+    </AppContainer>
   );
 }
+
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: 100vh;
+  width: 100vw;
+  background-color: red;
+`;
+
+const Tmp = styled.div`
+  width: calc(100% - 200px);
+  height: 100%;
+  background-color: rgb(18, 18, 18);
+`;
